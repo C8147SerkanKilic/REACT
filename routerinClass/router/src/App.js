b1f07017d63662e634c71d2fa5f5c66fa31355a1
C8147SerkanKilic <81584766+C8147SerkanKilic@users.noreply.github.com>
2021-08-31 23:47:00 +0300

    vov

---
+++ routerinClass/router/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './App.css'
+import Header from './components/Header'
+import About from "./pages/About"
+import { BrowserRouter as Router } from 'react-router-dom'
+
+const App = () => {
+  return (
+    <Router>
+    <div className= 'App' >
+      <Header/> 
+    </div>
+    </Router>
+  )
+}
+
+export default App
